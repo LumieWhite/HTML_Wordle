55b4b5c047c3b71464a29eda59c10ff820ea32a8
LumieWhite <lumiewhite@gmail.com>
2025-06-27 21:15:55 +0700

    setup 2

---
+++ init.js
@@ -0,0 +1,9 @@
+const initGameStyle = () => {
+	const root = document.documentElement.style;
+	root.setProperty("--ROWS", 4);
+	root.setProperty("--COLS", 4)
+}
+
+export const initGame = () => {
+
+}
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 import {mainLoop} from "./logic.js"
+import {initGame} from "./init.js"
 
+initGame();
 mainLoop();
